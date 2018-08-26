a1f6045dbeffe88290b8bcc85706b44d7eaf7daf
Mitchell "Mitch" Guthrie <mitchell.guthrie13@gmail.com>
2018-08-26 14:59:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+// Options
+const CLIENT_ID = '314803691631-2vd81iq646es4sj6vqj71mlfhrc4896d.apps.googleusercontent.com';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
+const defaultChannel = 'videogamedunkey'
+
+// Load auth2 library
+function handleClientLoad(){
+    gapi.load('client:auth2', initClient);
+}
+
+
+// Init API Client Library and set up silent listeners
+function initClient() {
+    gapi.client.init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    }).then(() => {
+        //Listen for client state changes
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        // Handle initial sign in state
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+//update UI sign in state changes
+function updateSigninStatus(isSignedIn) {
+    if(isSignedIn) {
+        authorizeButton.style.display = 'none';
+        singoutButton.style.display = 'block';
+        content.style.display = 'blcok';
+        videoContainer.style.display = 'block';
+        getChannel(defaultChannel);
+    } else {
+        authorizeButton.style.display = 'block';
+        singoutButton.style.display = 'none';
+        content.style.display = 'none';
+        videoContainer.style.display = 'none';
+    }
+}
+
+//Handle login
+function handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+//Handle logout
+function handleSignoutClick() {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+//get channel from api
+function getChannel(channel) {
+    console.log(channel);
+}
